e576cf023c6bfd1e768fb89eade8776d8ad1af1b
nguyenvubao1601 <22130024@st.hcmuaf.edu.vn>
2025-05-21 14:26:54 +0700

    ui detail movie

---
+++ app/src/main/java/com/example/movieapp/Activities/DetailActivity.java
@@ -0,0 +1,26 @@
+package com.example.movieapp.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.movieapp.R;
+
+public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_detail);
+
+
+
+    }
+}
